37c3ba560122e9b3478610927a32d30c367e266d
Niels <nielsburgler10@live.nl>
2024-05-02 16:28:06 +0200

    Worked on tfgnn model

--- code/predicting_model/Shift/DFTNN/shift_model_tfgnn.py
+++ code/predicting_model/Shift/DFTNN/shift_model_tfgnn.py
@@ -34,8 +34,21 @@ def edge_sets_fn(edge_set, *, edge_set_name):
     
     return features
 
+def set_initial_node_state(node_set, *, node_set_name):
+    # Since we only have one node set, we can ignore node_set_name.
+    if node_set_name == "atom":
+        return tf.keras.layers.Embedding(35, 256)(node_set["atom_num"])
+    return
 
-if __name__ == "__main__":
+def set_initial_edge_state(edge_set, *, edge_set_name):
+    features = edge_set.get_features_dict()
+
+    '''if edge_set_name == "bond":
+        distances = features.pop('distance')
+        features['rbf_distance'] = rbf_expansion(distances)'''
+    return tf.keras.layers.Embedding(3, 256)(edge_set["bond_type"])
+
+def build_model():
     batch_size = 32
     dataset = tf.data.TFRecordDataset(filenames=["data/own_data/shift_graph.tfrecords"])
     dataset = dataset.batch(batch_size)
@@ -49,21 +62,55 @@ if __name__ == "__main__":
 
     #preprocessing layers
     preproc_input = tf.keras.layers.Input(type_spec=preproc_input_spec)
-    graph = tfgnn.keras.layers.MapFeatures(node_sets_fn=node_sets_fn, edge_sets_fn=edge_sets_fn)(preproc_input)
+
+    graph = preproc_input.merge_batch_to_components() #might need this
+
+    graph = tfgnn.keras.layers.MapFeatures(node_sets_fn=set_initial_node_state, edge_sets_fn=set_initial_edge_state)(graph)
+    #TODO: no rbf expansion currently
+
     
-    #message passing layers
+    # Message passing layers
     for _ in range(3):
-        graph = tfgnn.keras.layers.NodeSetUpdate(
-            edge_set_inputs="bond",
-            next_state=tfgnn.keras.layers.Dense(256, use_bias=False)
+        node_message = tfgnn.keras.layers.GraphUpdate(
+            node_sets={"atom": tfgnn.keras.layers.NodeSetUpdate(
+                {"bond": tfgnn.keras.layers.SimpleConv(tf.keras.layers.Dense(256), "mean")},
+                next_state=tfgnn.keras.layers.NextStateFromConcat(tf.keras.layers.Dense(256, use_bias=False)))
+            }
         )(graph)
 
-        graph = tfgnn.keras.layers.EdgeSetUpdate(
+        # Edge Updating
+        # Turn into graph updates
+        edge_message = tfgnn.keras.layers.EdgeSetUpdate(
             next_state=tfgnn.keras.layers.NextStateFromConcat(
-                transformation= tfgnn.keras.layers.Dense(512, activation="softplus")
+                transformation= tf.keras.layers.Dense(512, activation="softplus")
             )
-        )(graph)
+        )(node_message)
+        edge_message = tfgnn.keras.layers.EdgeSetUpdate(next_state=tf.keras.layers.Dense(256))(edge_message)
+        edge_message = tfgnn.keras.layers.EdgeSetUpdate(next_state=tf.keras.layers.Dense(256, activation="softplus"))(edge_message)
+        edge_message = tfgnn.keras.layers.EdgeSetUpdate(next_state=tf.keras.layers.Dense(256, activation="softplus"))(edge_message)
+        edge_message = tf.keras.layers.Add()([graph, edge_message])
 
-        graph = tfgnn.keras.layers.EdgeSetUpdate(next_state=tfgnn.keras.layers.Dense(256))(graph)
-        graph = tfgnn.keras.layers.EdgeSetUpdate(next_state=tfgnn.keras.layers.Dense(256, activation="softplus"))(graph)
-        graph = tfgnn.keras.layers.EdgeSetUpdate(next_state=tfgnn.keras.layers.Dense(256, activation="softplus"))(graph)
+        graph_message = tf.keras.layers.Multiply()([node_message, edge_message])
+        graph_message = tf.keras.layers.AveragePooling2D()(graph_message)
+        graph_message = tfgnn.keras.layers.GraphUpdate(node_sets={"atom": tfgnn.keras.layers.NodeSetUpdate(
+            next_state= tf.keras.layers.Dense(256, activation="softplus")
+        )})(graph_message) # Not sure if edge states are also updated this way
+        graph_message = tfgnn.keras.layers.GraphUpdate(node_sets={"atom": tfgnn.keras.layers.NodeSetUpdate(
+            next_state= tf.keras.layers.Dense(256)
+        )})(graph_message)
+
+        graph = tf.keras.layers.Add()([graph, graph_message])
+
+    readout_features = tfgnn.keras.layers.Pool(
+        tfgnn.NODES, "mean", node_set_name="atom")(graph)
+    readout_features = tfgnn.keras.layers.StructuredReadout(readout_features)
+    logits = tf.keras.layers.Dense(256, activation="softplus")(readout_features)
+    logits = tf.keras.layers.Dense(256, activation="softplus")(logits)
+    logits = tf.keras.layers.Dense(128, activation="softplus")(logits)
+    output = tf.keras.layers.Dense(1, activation="softplus")(logits)
+
+    return tf.keras.Model(inputs=[preproc_input], outputs=[output])
+
+if __name__ == "__main__":
+    model = build_model()
+    print(model)
