753406facf0a2912ae6fdf483021f497f23bb399
Niels Burgler <nielsburgler10@live.nl>
2024-06-05 19:22:41 +0200

    fixed model loading

--- code/predicting_model/Shift/DFTNN/check_model.py
+++ code/predicting_model/Shift/DFTNN/check_model.py
@@ -4,31 +4,30 @@ import tensorflow_gnn as tfgnn
 import itertools
 from tensorflow_gnn import runner
 
-path = "/home/s3665828/Documents/Masters_Thesis/repo/CASCADE/code/predicting_model/Shift/DFTNN/"
+path = "C:/Users/niels/Documents/repo/CASCADE/code/predicting_model/Shift/DFTNN/"
 
-train_ds_provider = runner.TFRecordDatasetProvider(filenames=["data/own_data/shift_train.tfrecords"])
-dataset = tf.data.TFRecordDataset(filenames=["data/own_data/shift_train.tfrecords"])
-graph_schema = tfgnn.read_schema("code/predicting_model/GraphSchema.pbtxt")
-graph_tensor_spec = tfgnn.create_graph_spec_from_schema_pb(graph_schema)
-
+model = tf.saved_model.load(path + "tmp/gnn_model/export")
+signature_fn = model.signatures[
+    tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY]
 
 def _clean_example_for_serving(graph_tensor):
-  graph_tensor = graph_tensor.remove_features(node_sets={"_readout": ["shift"]})
+  #graph_tensor = graph_tensor.remove_features(node_sets={"paper": ["label"]})
+  print(graph_tensor)
   serialized_example = tfgnn.write_example(graph_tensor)
   return serialized_example.SerializeToString()
 
 num_examples = 2
-dataset = train_ds_provider.get_dataset(tf.distribute.InputContext())
+#dataset = tf.data.TFRecordDataset(filenames=["data/own_data/shift_train.tfrecords"])
+dataset_provider = runner.TFRecordDatasetProvider(filenames=["data/own_data/shift_train.tfrecords"])
+dataset = dataset_provider.get_dataset(tf.distribute.InputContext())
+print(dataset)
 serialized_examples = [_clean_example_for_serving(gt)
-                        for gt in itertools.islice(dataset, num_examples)]
+                       for gt in itertools.islice(dataset, num_examples)]
 
-dataset = tf.data.Dataset.from_tensor_slices(serialized_examples)
+ds = tf.data.Dataset.from_tensor_slices(serialized_examples)
 
-model = tf.saved_model.load(path + "gnn/models/")
-signature_fn = model.signatures[
-    tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY]
-input_dict = {"examples": next(iter(dataset.batch(2)))}
+input_dict = {"examples": next(iter(ds.batch(2)))}
 
 output_dict = signature_fn(**input_dict)
-logits = output_dict["shift"]
+logits = output_dict["shifts"]
 #print(logits)
