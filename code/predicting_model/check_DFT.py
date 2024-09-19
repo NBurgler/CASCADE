97393faa2aeb85b854c7072311bd4965ad4de74a
Niels <nielsburgler10@live.nl>
2024-09-19 16:17:11 +0200

    Added CASCADE data support

--- code/predicting_model/check_DFT.py
+++ code/predicting_model/check_DFT.py
@@ -4,14 +4,23 @@ from rdkit.Chem import AllChem
 import gzip
 
 if __name__ == "__main__":
-    path = "C:/Users/niels/Documents/repo/CASCADE/"
+    #path = "C:/Users/niels/Documents/repo/CASCADE/"
+    path = "/home/s3665828/Documents/Masters_Thesis/repo/CASCADE/"
+    
     shift_df = pd.read_csv(path + "data/DFT8K/DFT8K.csv.gz", index_col=0)
     print(shift_df)
+    mol_id = 20176935
+    print(shift_df.loc[(shift_df['mol_id'] == mol_id) & (shift_df['atom_index'] == 2)]["Shift"])
 
     with gzip.open(path + "data/DFT8K/DFT.sdf.gz", 'rb') as dft:
-        text = dft.read()
-        textSplit = text.split("\n")
-        print(textSplit[0])
-        mol_suppl = Chem.ForwardSDMolSupplier(dft)
-        for mol in mol_suppl:
-            print(Chem.MolToSmiles(mol))
+        mol_suppl = Chem.ForwardSDMolSupplier(dft, removeHs=False)
+        #for mol in mol_suppl:
+            #print(Chem.MolToSmiles(mol))
+            #print(mol.GetProp("_Name"))
+            #print(Chem.Get3DDistanceMatrix(mol))
+            #for atom in mol.GetAtoms():
+                #print(atom.GetIdx())
+                #print(atom.GetSymbol())
+
+
+    #print(shift_df.loc[shift_df['mol_id'] == 11224])
--- code/predicting_model/create_graph_tensor.py
+++ code/predicting_model/create_graph_tensor.py
@@ -5,6 +5,7 @@ import tensorflow_gnn as tfgnn
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
+import gzip
 import os
 
 from tqdm import tqdm
@@ -119,7 +120,6 @@ def processData(filepath, path):
 
             mol_id += 1
 
-    
 
     mol_df = pd.DataFrame.from_dict(mol_dict)
     atom_df = pd.DataFrame.from_dict(atom_dict)
@@ -130,6 +130,75 @@ def processData(filepath, path):
     mol_df.to_csv(path + "code/predicting_model/Shift/DFTNN/small_data_mol.csv.gz", compression='gzip')
     atom_df.to_csv(path + "code/predicting_model/Shift/DFTNN/small_data_atom.csv.gz", compression='gzip')
     bond_df.to_csv(path + "code/predicting_model/Shift/DFTNN/small_data_bond.csv.gz", compression='gzip')
+
+def processDFTData(path):
+
+    #print(shift_df.loc[(shift_df['mol_id'] == 2192)]["Shift"])
+
+    mol_dict = {"mol_id":[], "smiles":[], "n_atoms":[], "n_bonds":[], "n_pro":[]}
+    atom_dict = {"mol_id":[], "atom_symbol":[], "chiral_tag":[], "degree":[], 
+                 "formal_charge":[], "hybridization":[], "is_aromatic":[], 
+                 "no_implicit":[], "num_Hs":[], "valence":[], "Shift":[]}
+    bond_dict = {"mol_id":[], "bond_type":[], "distance":[], "is_conjugated":[], 
+                 "stereo":[], "source":[], "target":[]}
+
+    with gzip.open(path + "data/DFT8K/DFT.sdf.gz", 'rb') as dft:
+        shift_df = pd.read_csv(path + "data/DFT8K/DFT8K.csv.gz", index_col=0)
+        mol_suppl = Chem.ForwardSDMolSupplier(dft, removeHs=False)
+        for mol in mol_suppl:
+            n_atoms = mol.GetNumAtoms(onlyExplicit=False)
+            n_bonds = mol.GetNumBonds(onlyHeavy=False)
+            mol_id = int(mol.GetProp("_Name"))
+
+            mol_dict["mol_id"].append(mol_id)
+            mol_dict["smiles"].append(Chem.MolToSmiles(mol))
+            mol_dict["n_atoms"].append(n_atoms)
+            mol_dict["n_bonds"].append(n_bonds)
+            mol_dict["n_pro"].append(n_atoms - mol.GetNumHeavyAtoms())
+
+            iter_H = 0
+            distance_matrix = Chem.Get3DDistanceMatrix(mol)
+        
+            for n, atom in enumerate(mol.GetAtoms()):
+                atom_dict["mol_id"].append(mol_id)
+                atom_symbol = atom.GetSymbol()
+                atom_dict["atom_symbol"].append(atom_symbol)
+                atom_dict["chiral_tag"].append(atom.GetChiralTag())
+                atom_dict["degree"].append(atom.GetTotalDegree())
+                atom_dict["formal_charge"].append(atom.GetFormalCharge() + 1)   # Now ranges from 0...2 instead of -1...1
+                atom_dict["hybridization"].append(atom.GetHybridization())
+                atom_dict["is_aromatic"].append(atom.GetIsAromatic())
+                atom_dict["no_implicit"].append(atom.GetNoImplicit())
+                atom_dict["valence"].append(atom.GetTotalValence())
+                atom_dict["num_Hs"].append(atom.GetTotalNumHs(includeNeighbors=True))
+                
+                if (atom_symbol == "H"):
+                    atom_dict["Shift"].append(shift_df.loc[(shift_df['mol_id'] == mol_id) & (shift_df['atom_index'] == n)]["Shift"].values[0])
+                else: # not really necessary now, but useful for adding C-NMR in the future
+                    atom_dict["Shift"].append(0.0)  # 0.0 shift for non-H atoms
+
+            for n, bond in enumerate(mol.GetBonds()):
+                bond_dict["mol_id"].append(mol_id)
+                bond_dict["bond_type"].append(bond.GetBondType())
+
+                bond_source = bond.GetBeginAtomIdx()
+                bond_target = bond.GetEndAtomIdx()
+                bond_dict["distance"].append(distance_matrix[bond_source][bond_target])
+                bond_dict["source"].append(bond_source)
+                bond_dict["target"].append(bond_target)
+                bond_dict["is_conjugated"].append(bond.GetIsConjugated())
+                bond_dict["stereo"].append(bond.GetStereo())
+
+
+    mol_df = pd.DataFrame.from_dict(mol_dict)
+    atom_df = pd.DataFrame.from_dict(atom_dict)
+    bond_df = pd.DataFrame.from_dict(bond_dict)
+
+    bond_df["norm_distance"] = (bond_df["distance"] - bond_df["distance"].mean())/bond_df["distance"].std()
+
+    mol_df.to_csv(path + "code/predicting_model/Shift/DFTNN/DFT_data_mol.csv.gz", compression='gzip')
+    atom_df.to_csv(path + "code/predicting_model/Shift/DFTNN/DFT_data_atom.csv.gz", compression='gzip')
+    bond_df.to_csv(path + "code/predicting_model/Shift/DFTNN/DFT_data_bond.csv.gz", compression='gzip')
 
 
 def one_hot_encode_atoms(atom_symbols):
@@ -143,9 +212,17 @@ def one_hot_encode_atoms(atom_symbols):
             index = 2
         elif symbol == "N":
             index = 3
+        elif symbol == "F":
+            index = 4
+        elif symbol == "Cl":
+            index = 5
+        elif symbol == "S":
+            index = 6
+        elif symbol == "P":
+            index = 7        
         indices = np.append(indices, index)
 
-    return tf.one_hot(tf.convert_to_tensor(indices), 4)
+    return tf.one_hot(tf.convert_to_tensor(indices), 7)
 
 def one_hot_encode_shape(shape_symbols):            # The output will be a matrix of six one-hots, where each one-hot encodes for a shape
     one_hot_shapes = np.empty((0,6,8), dtype=int)   # i.e. 'dtp' will be encoded as matrix of six one-hots where the first one encodes
@@ -279,11 +356,50 @@ def create_graph_tensor_shape(mol_data, atom_data, bond_data):
 
     return graph_tensor
 
+def create_DFT_tensors(path):
+    processDFTData(path)
+
+    mol_df = pd.read_csv(path + "code/predicting_model/Shift/DFTNN/DFT_data_mol.csv.gz", index_col=0)
+    atom_df = pd.read_csv(path + "code/predicting_model/Shift/DFTNN/DFT_data_atom.csv.gz", index_col=0)
+    bond_df = pd.read_csv(path + "code/predicting_model/Shift/DFTNN/DFT_data_bond.csv.gz", index_col=0)
+
+    train_data = tf.io.TFRecordWriter(path + "data/own_data/DFT_data_train.tfrecords")
+    test_data = tf.io.TFRecordWriter(path + "data/own_data/DFT_data_test.tfrecords")
+    valid_data = tf.io.TFRecordWriter(path + "data/own_data/DFT_data_valid.tfrecords")
+    all_data = tf.io.TFRecordWriter(path + "data/own_data/all_DFT_data.tfrecords")
+
+    total = len(mol_df)
+
+    for idx, mol_id in tqdm(enumerate(mol_df["mol_id"])):
+        mol_data = mol_df.loc[mol_df["mol_id"] == mol_id]
+        atom_data = atom_df.loc[atom_df["mol_id"] == mol_id]
+        bond_data = bond_df.loc[bond_df["mol_id"] == mol_id]
+
+        if (mol_data['n_pro'].values == 0):
+            continue
+        
+        # make sure the index resets to 0 instead of continuing from the previous molecule
+        atom_data = atom_data.reset_index(drop=True)
+        graph_tensor = create_graph_tensor_shift(mol_data, atom_data, bond_data)
+        example = tfgnn.write_example(graph_tensor)
+        
+        all_data.write(example.SerializeToString())
+        if idx < total*0.7:
+            train_data.write(example.SerializeToString())
+        elif idx < total*0.85:
+            valid_data.write(example.SerializeToString())
+        else:
+            test_data.write(example.SerializeToString())
+
+
 if __name__ == "__main__":
     #path = "/home1/s3665828/code/CASCADE/"
-    #path = "/home/s3665828/Documents/Masters_Thesis/repo/CASCADE/"
-    path = "C:/Users/niels/Documents/repo/CASCADE/"
+    path = "/home/s3665828/Documents/Masters_Thesis/repo/CASCADE/"
+    #path = "C:/Users/niels/Documents/repo/CASCADE/"
 
+    create_DFT_tensors(path)
+
+    '''
     # create dataframes if they do not exist yet
     if not os.path.isfile(path + "code/predicting_model/Shift/DFTNN/small_data_mol.csv.gz"):
         processData(path + 'data/own_data/small_dataset.txt', path)
@@ -324,3 +440,4 @@ if __name__ == "__main__":
             valid_data.write(example.SerializeToString())
         else:
             test_data.write(example.SerializeToString())
+'''
